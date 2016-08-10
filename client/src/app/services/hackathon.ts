c35e86ec2e2edaced5f8f124c136ff8d18de3225
kaeku314@gmail.com <kaeku314@gmail.com>
2016-08-10 18:57:06 +0200

    add more stuff to hackathon card view

--- client/src/app/services/hackathon.ts
+++ client/src/app/services/hackathon.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HackathonModel} from '../models/hackathon';
 import {Http} from '@angular/http';
+import {Observable} from 'rxjs';
 
 const hackathonsMock: HackathonModel[] = [
 	{ id: 0, name: 'First Hackathon' },
@@ -14,7 +15,7 @@ export class HackathonService {
 
     }
 
-	getHackathons() {
+	getHackathons(): Observable<any> {
 		return this._http.get('http://localhost:3000/hackathons');
 	}
 }
