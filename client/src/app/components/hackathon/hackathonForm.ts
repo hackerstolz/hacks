553f38f28e78bb04ba5fb46e9269e8952a7eba3b
kaeku314@gmail.com <kaeku314@gmail.com>
2016-08-11 13:16:20 +0200

    add loading animation

--- client/src/app/components/hackathon/hackathonForm.ts
+++ client/src/app/components/hackathon/hackathonForm.ts
@@ -10,17 +10,27 @@ import {Router} from '@angular/router';
 })
 export class HackathonFormComponent {
     private hackathon: HackathonModel;
+    private _submitted: boolean = false;
 
     constructor(private _hackathonService: HackathonService, private _router: Router) {
         this.hackathon = new HackathonModel();
     }
 
     onSubmit() {
+        this._submitted = true;
         this._hackathonService.createHackathon(this.hackathon)
             .subscribe((res) => {
-                if(res.status === 200) {
+                if (res.status === 200) {
                     this._router.navigateByUrl('/dashboard');
                 }
+            }, (err) => {
+                console.log(err);
+                this._submitted = false;
             });
+
+    }
+
+    get submitted(): boolean {
+        return this._submitted;
     }
 }
